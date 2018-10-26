a37df661d32826a698454a002bcb557f109f29b4
angelinn <angelin.nedelchev@outlook.com>
2018-10-26 22:26:40 +0300

    Add button text bindable property.

--- src/Xam.Forms.UpdatePrompt/UpdatePrompt.xaml.cs
+++ src/Xam.Forms.UpdatePrompt/UpdatePrompt.xaml.cs
@@ -12,9 +12,25 @@ namespace Xam.Forms.UpdatePrompt
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class UpdatePrompt : Grid
 	{
+        private static Button button;
 		public UpdatePrompt ()
 		{
 			InitializeComponent ();
+            button = btn;
 		}
-	}
+
+        public static readonly BindableProperty ButtonTextProperty =
+            BindableProperty.Create("ButtonText", typeof(string), typeof(UpdatePrompt), null, propertyChanged: OnButtonTextChanged);
+        
+        public string ButtonText
+        {
+            get { return (string)GetValue(ButtonTextProperty); }
+            set { SetValue(ButtonTextProperty, value); }
+        }
+
+        private static void OnButtonTextChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            button.Text = newValue.ToString();
+        }
+    }
 }
