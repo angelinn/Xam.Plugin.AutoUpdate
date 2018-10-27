94e03562e424af886b4fce9b9931e30fc968ead1
angelinn <angelin.nedelchev@outlook.com>
2018-10-27 10:06:25 +0300

    Add separate parameters object.

---
+++ src/Xam.Plugin.CheckForUpdates/UpdateManagerParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xam.Plugin.CheckForUpdates
+{
+    public class UpdateManagerParameters
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public string Confirm { get; set; }
+        public string Cancel { get; set; }
+        public Func<Task<UpdatesCheckResponse>> CheckForUpdatesFunction { get; set; }
+        public TimeSpan RunEvery { get; set; }
+    }
+}
