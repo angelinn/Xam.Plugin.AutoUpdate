89b688ee2150deff0a1cb12ae146dcdf02f56c5b
angelinn <angelin.nedelchev@outlook.com>
2018-10-27 10:25:33 +0300

    Create new projects.

---
+++ src/Xam.Plugin.AutoUpdate/Services/IFileOpener.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xam.Plugin.AutoUpdate.Services
+{
+    public interface IFileOpener
+    {
+        void OpenFile(string path);
+    }
+}
