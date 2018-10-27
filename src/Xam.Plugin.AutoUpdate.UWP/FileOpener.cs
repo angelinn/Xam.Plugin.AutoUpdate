efbfad51a214124028bd090220a8dc563676dfe6
angelinn <angelin.nedelchev@outlook.com>
2018-10-27 13:25:22 +0300

    Add UWP implementation.

---
+++ src/Xam.Plugin.AutoUpdate.UWP/FileOpener.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Windows.Storage;
+using Xam.Plugin.AutoUpdate.Services;
+using Xam.Plugin.AutoUpdate.UWP;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(FileOpener))]
+namespace Xam.Plugin.AutoUpdate.UWP
+{
+    public class FileOpener : IFileOpener
+    {
+        public async void OpenFile(byte[] data, string name)
+        {
+            string directory = ApplicationData.Current.LocalFolder.Path;
+            foreach (string file in Directory.GetFiles(directory))
+            {
+                if (Path.GetExtension(file) == "*.appxbundle")
+                    File.Delete(file);
+            }
+
+            StorageFile sampleFile = await ApplicationData.Current.LocalFolder.CreateFileAsync(name);
+            await FileIO.WriteBytesAsync(sampleFile, data);
+            await Windows.System.Launcher.LaunchFileAsync(sampleFile);
+        }
+    }
+}
