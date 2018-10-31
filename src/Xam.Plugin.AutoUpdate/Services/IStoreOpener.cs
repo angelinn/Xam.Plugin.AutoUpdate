9b801a800504e47ca34250578ca588838e93f23e
angelinn <angelin.nedelchev@outlook.com>
2018-10-31 12:05:33 +0200

    Add IStoreOpener.

---
+++ src/Xam.Plugin.AutoUpdate/Services/IStoreOpener.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xam.Plugin.AutoUpdate.Services
+{
+    public interface IStoreOpener
+    {
+        void OpenStore();
+    }
+}
