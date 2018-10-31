25d56b90dd4a58dbacb665a83011e80398c47a72
angelinn <angelin.nedelchev@outlook.com>
2018-10-31 12:09:51 +0200

    Add UWP StoreOpener implementation.

---
+++ src/Xam.Plugin.AutoUpdate.UWP/MicrosoftStoreOpener.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.System;
+using Xam.Plugin.AutoUpdate.Services;
+
+namespace Xam.Plugin.AutoUpdate.UWP
+{
+    public class MicrosoftStoreOpener : IStoreOpener
+    {
+        public async void OpenStore()
+        {
+            await Launcher.LaunchUriAsync(new Uri($"ms-windows-store://pdp/?ProductId={Package.Current.Id.FamilyName}"));
+        }
+    }
+}
