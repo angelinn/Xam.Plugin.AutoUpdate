82786031d0e16dc71c248f48aed18ed87968ef03
angelinn <angelin.nedelchev@outlook.com>
2018-10-31 12:18:33 +0200

    Add potential iOS StoreOpener implementation.

---
+++ src/Xam.Plugin.AutoUpdate.iOS/AppStoreOpener.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xam.Plugin.AutoUpdate.Services;
+
+namespace Xam.Plugin.AutoUpdate.iOS
+{
+    public class AppStoreOpener : IStoreOpener
+    {
+        public void OpenStore()
+        {
+            NSString appID = NSBundle.MainBundle.InfoDictionary["CFBundleIdentifier"] as NSString;
+            UIApplication.SharedApplication.OpenUrl(new NSUrl($"itms://itunes.apple.com/us/app/apple-store/{appID}?mt=8"));
+        }
+    }
+}
